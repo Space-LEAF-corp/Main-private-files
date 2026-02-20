59b995c046dd08320854ff9eaefd9ccf1b7f0be1
Leif W. Sogge <144378178+GuardianNinja@users.noreply.github.com>
2026-02-20 10:28:24 -0500

    Create validate_user.py
    Signed-off-by: Leif W. Sogge <144378178+GuardianNinja@users.noreply.github.com>

---
+++ GQI-EA/src/api/validate_user.py
@@ -0,0 +1,11 @@
+"""
+User Validation API
+-------------------
+Validates whether a user is authorized to submit inquiries.
+"""
+
+def validate_user(username: str) -> bool:
+    """
+    Placeholder validation logic.
+    """
+    return True if username else False
