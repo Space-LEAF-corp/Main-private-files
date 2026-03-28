259ea19f94933eda024aba12275db5ee29543b6e
Leif W. Sogge <144378178+GuardianNinja@users.noreply.github.com>
2026-03-28 16:57:21 -0400

    Update main.rs

--- raptor-os/src/main.rs
+++ raptor-os/src/main.rs
@@ -10,7 +10,6 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // Boot sequence for Raptor OS
     config::init();
     turbo_stack::init_turbo_stack();
     shadow_panther::init_shadow_panther_copilot();
