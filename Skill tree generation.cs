c1deddceebee2b9b56d5829ab8a3ded4443b0b96
Leif W. Sogge <144378178+GuardianNinja@users.noreply.github.com>
2026-03-29 18:23:27 -0400

    Create Skill tree generation.cs

---
+++ Skill tree generation.cs
@@ -0,0 +1,9 @@
+public class SkillNode
+{
+    public string Id;
+    public string Name;
+    public string Description;
+    public int Tier;
+    public string[] PrerequisiteIds;
+    public bool Unlocked;
+}
