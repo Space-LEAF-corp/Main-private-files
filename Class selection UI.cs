46ef0076a2307996f73e4f519a58ccbe5bac8742
Leif W. Sogge <144378178+GuardianNinja@users.noreply.github.com>
2026-03-29 18:21:38 -0400

    Create Class selection UI.cs

---
+++ Class selection UI.cs
@@ -0,0 +1,9 @@
+public class ClassOption
+{
+    public ClassType Class;
+    public string DisplayName;
+    public string Description;
+    public int BaseStr;
+    public int BaseAgi;
+    public int BaseInt;
+}
