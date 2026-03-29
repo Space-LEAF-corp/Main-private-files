49327b6e491534b99b0148ad0114ccd745585660
Leif W. Sogge <144378178+GuardianNinja@users.noreply.github.com>
2026-03-29 18:22:17 -0400

    Create Race + class synergy rules.cs

---
+++ Race + class synergy rules.cs
@@ -0,0 +1,9 @@
+public class RaceClassSynergy
+{
+    public Race Race;
+    public ClassType Class;
+    public int BonusStrength;
+    public int BonusAgility;
+    public int BonusIntelligence;
+    public string SynergyTag; // e.g. "Panther Bond", "Ancient Lineage"
+}
